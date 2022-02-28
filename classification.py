8552da2fcb55bc81cfefb5baf29b8da43ab318e8
aledelmo <delmonte.ale92@gmail.com>
2022-02-28 11:19:45 +0100

    first working model

--- classification.py
+++ classification.py
@@ -29,14 +29,10 @@ x_dataset = np.array(x_dataset)[:, :21, :2]
 x_dataset = np.reshape(x_dataset, (x_dataset.shape[0], x_dataset.shape[1] * x_dataset.shape[2]))
 y_dataset = np.array(y_dataset)
 
-x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset, train_size=0.75, random_state=RANDOM_SEED)
+'--------------------------------'
 
-'''
-tf.keras.layers.Dropout(0.2),
-tf.keras.layers.Dense(20, activation='relu'),
-tf.keras.layers.Dropout(0.4),
-tf.keras.layers.Dense(10, activation='relu'),
-'''
+x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset, train_size=0.75,
+                                                    random_state=RANDOM_SEED)
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Input((21 * 2, )),
@@ -50,28 +46,17 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
     model_save_path, verbose=1, save_weights_only=False)
 es_callback = tf.keras.callbacks.EarlyStopping(patience=20, verbose=1)
 
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
-)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-model.fit(
-    x_train,
-    y_train,
-    epochs=1000,
-    batch_size=128,
-    validation_data=(x_test, y_test),
-    callbacks=[cp_callback, es_callback]
-)
+model.fit(x_train, y_train, epochs=1000, batch_size=128, validation_data=(x_test, y_test),
+          callbacks=[cp_callback, es_callback])
 
 val_loss, val_acc = model.evaluate(x_test, y_test, batch_size=128)
 
 model = tf.keras.models.load_model(model_save_path)
 
-predict_result = model.predict(np.array([x_test[0]]))
-print(np.squeeze(predict_result))
-print(np.argmax(np.squeeze(predict_result)))
+'--------------------------------'
 
 
 def print_confusion_matrix(y_true, _y_pred, report=True):
@@ -89,7 +74,7 @@ def print_confusion_matrix(y_true, _y_pred, report=True):
         print('Classification Report')
         print(classification_report(y_test, _y_pred))
 
-print(x_test)
+
 y_pred = model.predict(x_test)
 y_pred = np.argmax(y_pred, axis=1)
 
--- prediction.py
+++ prediction.py
@@ -11,6 +11,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import tensorflow as tf
+from os import system
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -23,8 +24,9 @@ actions = {0: 'palm', 1: 'OK', 2: 'fist'}
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
-
-    with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    lum = 1
+    tracking = False
+    with mp_hands.Hands(model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
         while cap.isOpened():
             success, image = cap.read()
             if not success:
@@ -40,34 +42,64 @@ if __name__ == '__main__':
             h, w, c = image.shape
             frame_landmarks = np.zeros(shape=(42, 3))
 
+            system('say Hello world!')
+
             if results.multi_hand_landmarks:
                 if 0 < len(results.multi_hand_landmarks) < 3:
                     for j, hand in enumerate(results.multi_hand_landmarks):
                         for i, landmark in enumerate(hand.landmark):
                             frame_landmarks[21*j+i][0] = int(landmark.x * w)
                             frame_landmarks[21*j+i][1] = int(landmark.y * h)
-                            frame_landmarks[21*j+i][2] = landmark.z
+                            frame_landmarks[21*j+i][2] = landmark.z * w
 
-                    _max = np.amax(frame_landmarks[:21], axis=0)
-                    _min = np.amin(frame_landmarks[:21], axis=0)
-
+                    _max = np.amax(frame_landmarks, axis=0)
+                    _min = np.amin(frame_landmarks, axis=0)
                     frame_landmarks[:, -1] = 255 * ((frame_landmarks[:, -1] - _min[-1]) / (_max[-1] - _min[-1]))
-
-                    bbox = cv2.rectangle(image, (int(_min[0]), int(_min[1])), (int(_max[0]), int(_max[1])),
-                                         (255, 0, 0), 3)
 
                     for lm in frame_landmarks:
                         cv2.circle(image, (int(lm[0]), int(lm[1])), 8, thickness=-1, color=[lm[2]] * 3)
 
+                    _max1 = np.amax(frame_landmarks[:21], axis=0)
+                    _min1 = np.amin(frame_landmarks[:21], axis=0)
+                    cv2.rectangle(image, (int(_min1[0]), int(_min1[1])), (int(_max1[0]), int(_max1[1])),
+                                         (255, 0, 0), 3)
+
+                    _max2 = np.amax(frame_landmarks[21:], axis=0)
+                    _min2 = np.amin(frame_landmarks[21:], axis=0)
+                    cv2.rectangle(image, (int(_min2[0]), int(_min2[1])), (int(_max2[0]), int(_max2[1])),
+                                         (255, 0, 0), 3)
+
                     image = cv2.flip(image, 1)
 
-                    to_predict = frame_landmarks[:21, :2]
-                    to_predict = (to_predict - to_predict[0]).flatten()
+                    frame_landmarks -= frame_landmarks[0]
+
+                    to_predict = frame_landmarks[:21, :2].flatten()
                     to_predict = np.reshape(to_predict, (1, to_predict.shape[0]))
                     predict_result = model.predict(to_predict)
 
-                    text = cv2.putText(image, actions[np.argmax(predict_result)], (w - int(_min[0]), int(_min[1])),
+                    cv2.putText(image, actions[np.argmax(predict_result)], (w - int(_min1[0]), int(_min1[1])),
                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+                    to_predict = frame_landmarks[21:, :2].flatten()
+                    to_predict = np.reshape(to_predict, (1, to_predict.shape[0]))
+                    predict_result2 = model.predict(to_predict)
+
+                    cv2.putText(image, actions[np.argmax(predict_result2)], (w - int(_min2[0]), int(_min2[1])),
+                                       cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+                    if len(results.multi_hand_landmarks) == 2 :
+                        if  np.argmax(predict_result2) == 2 and np.argmax(predict_result) == 0:
+                            if tracking:
+                                image += lum
+                                lum += 5
+                            tracking = True
+                        else:
+                            tracking = False
+                            lum = 1
+                    else:
+                        tracking = False
+                        lum = 1
+
             else:
                 image = cv2.flip(image, 1)
 
